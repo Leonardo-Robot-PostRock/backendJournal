bb299b96f329430b8df236ce833677e77085f4f7
Leonardo <leonardo38361@gmail.com>
2024-07-22 21:25:59 -0300

    configuración básica de routes para imagenes

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const imageRoutes = require('./routes/imageRoutes');
+
+app.use(express.json());
+app.use('/images', imageRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
